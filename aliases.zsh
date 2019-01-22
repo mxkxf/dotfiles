776b326da6a8a27c60dca379d1e8f537227cfe06
Mike Francis <mikeffrancis@gmail.com>
2019-01-22 10:53:57 +0000

    Add cool MySQL container command

--- aliases.zsh
+++ aliases.zsh
@@ -6,3 +6,7 @@ alias ohmyzsh="vi ~/.oh-my-zsh"
 # Git
 
 alias gphm="git push heroku master"
+
+# MySQL
+
+alias new_mysql_container="docker run -d --rm -p 33060:3306 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=test mysql:5 --default-authentication-plugin=mysql_native_password"
