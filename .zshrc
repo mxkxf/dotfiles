063cb344cb42a052656c0b22c326e7d26687f341
Mike Francis <mikeffrancis@gmail.com>
2020-09-21 11:49:51 +0100

    Latest updates

--- .zshrc
+++ .zshrc
@@ -7,7 +7,7 @@ export ZSH=/Users/mikefrancis/.oh-my-zsh
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-ZSH_THEME="agnoster"
+ZSH_THEME="kolo"
 
 DEFAULT_USER=$USER
 
@@ -76,3 +76,4 @@ source $ZSH/oh-my-zsh.sh
 
 # ssh
 # export SSH_KEY_PATH="~/.ssh/rsa_id"
+export PATH="/usr/local/opt/terraform@0.11/bin:$PATH"
--- aliases.zsh
+++ aliases.zsh
@@ -9,11 +9,18 @@ alias gphm="git push heroku master"
 
 # k8s
 
-alias ku="kubectl"
+alias ku="kubectl -n default"
+alias kup="kubectl -n prod"
 
 # MySQL
 
 function new_mysql_container() {
   local port="${2:-3306}"
-  docker run -d --rm -p $port:3306 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE="$1" --name="$1_db" mysql:5 --default-authentication-plugin=mysql_native_password
+  docker run -d --rm -p $port:3306 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE="$1" --name="$1_db" mysql:8 --default-authentication-plugin=mysql_native_password
 }
+
+# Docker
+
+alias dcu="docker-compose up --build"
+alias dcd="docker-compose down"
+
