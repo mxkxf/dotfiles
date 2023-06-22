87464faa47ecf124ecfca4a7c4787e77af8dbdf5
Mike Francis <mikeffrancis@gmail.com>
2023-06-22 17:18:12 +0100

    Updated new_mysql_container with volume

--- aliases.zsh
+++ aliases.zsh
@@ -6,11 +6,11 @@ alias nrd="npm run dev"
 alias zshconfig="vi ~/.zshrc"
 alias ohmyzsh="vi ~/.oh-my-zsh"
 
-# MySQL
+# Databases
 
 function new_mysql_container() {
   local port="${2:-3306}"
-  docker run -d --rm -p $port:3306 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE="$1" --name="$1_db" mysql:8 --default-authentication-plugin=mysql_native_password
+  docker run -d --rm --name="$1_db" -p $port:3306 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE="$1" -v "$1_db:/var/lib/mysql" mysql:8 --default-authentication-plugin=mysql_native_password
 }
 
 # Docker
@@ -20,5 +20,9 @@ alias dcd="docker-compose down"
 
 # Python
 
-alias python=/usr/local/bin/python3
-alias pip=/usr/local/bin/pip3
+alias python=/opt/homebrew/bin/python3.11
+alias pip=/usr/local/bin/pip3.11
+
+# k8s 
+
+alias ku="kubectl"
