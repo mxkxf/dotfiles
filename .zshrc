ed4ed15bd4f8d571269d350f67a47672198cbff3
Mike Francis <mikeffrancis@gmail.com>
2024-10-25 15:47:49 +0100

    moar bonks

--- .zshrc
+++ .zshrc
@@ -53,11 +53,14 @@ ZSH_CUSTOM=$DOTFILES
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git)
+plugins=(git fzf history thefuck z)
 
 source $ZSH/oh-my-zsh.sh
 
 # User configuration
+
+# export FZF_BASE=$ZSH/plugins/fzf
+source <(fzf --zsh)
 
 # export MANPATH="/usr/local/man:$MANPATH"
 
--- aliases.zsh
+++ aliases.zsh
@@ -1,3 +1,9 @@
+alias ls="lsd"
+alias l="ls -l"
+alias la="ls -a"
+alias lla="ls -la"
+alias lt="ls --tree"
+
 # Testing bun as a node replacement
 
 alias ni="bun install"
@@ -6,25 +12,33 @@ alias nrb="bun run build"
 alias nrs="bun storybook"
 alias nrt="bun test"
 
-alias megaupdate="brew update && brew upgrade && brew upgrade --cask --greedy && brew autoremove && brew cleanup"
+alias megaupdate="
+  omz update && \
+  brew update && \
+  brew upgrade && \
+  brew upgrade --cask --greedy && \
+  brew autoremove && \
+  brew cleanup"
 
 # ZSH
 
 alias zshconfig="vi ~/.zshrc"
 alias ohmyzsh="vi ~/.oh-my-zsh"
 
-# Databases
-
-function new_mysql_container() {
-  local port="${2:-3306}"
-  docker run -d --rm --name="$1_db" -p $port:3306 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE="$1" -v "$1_db:/var/lib/mysql" mysql:8 --default-authentication-plugin=mysql_native_password
-}
-
 # Docker
 
 alias dcu="docker-compose up --build"
 alias dcd="docker-compose down"
 
+function newpgsql() {
+  local port="${2:-5432}"
+  docker run -d --rm --name="$1" -p $port:5432 -e POSTGRES_DB="$1" -e POSTGRES_PASSWORD=mysecretpassword -v "$1:/var/lib/mysql" postgres
+}
+
 # k8s 
 
 alias ku="kubectl"
+
+# lol
+
+alias tmrw="gdate -d '+1 day' '+%s'"
